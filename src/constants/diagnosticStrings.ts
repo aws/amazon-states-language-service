4093cf0c9dee962f6a3cfc4dead4abc98a5d8bde
Alan Bogusiewicz <alabogus@amazon.com>
2020-06-29 13:03:36 -0700

    Adds a missing . to validation message

--- src/constants/diagnosticStrings.ts
+++ src/constants/diagnosticStrings.ts
@@ -7,7 +7,7 @@ export const MESSAGES = {
     INVALID_NEXT: 'The value of "Next" property must be the name of an existing state.',
     INVALID_DEFAULT: 'The value of "Default" property must be the name of an existing state.',
     INVALID_START_AT: 'The value of "StartAt" property must be the name of an existing state.',
-    INVALID_JSON_PATH: 'The value for the field must be a valid JSONPath',
+    INVALID_JSON_PATH: 'The value for the field must be a valid JSONPath.',
     UNREACHABLE_STATE: 'The state cannot be reached. It must be referenced by at least one other state.',
     NO_TERMINAL_STATE: 'No terminal state. The state machine must have at least one terminal state (a state in which the "End" property is set to true).',
     INVALID_PROPERTY_NAME: 'Field is not supported.',
